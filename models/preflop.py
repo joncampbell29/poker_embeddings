9033da407c1abbde1d41f639d05fb643721e2ee5
Jon Campbell <campbelljonjr@comcast.net>
2025-04-08 13:14:19 -0400

    mdoel update

--- models/preflop.py
+++ models/preflop.py
@@ -29,4 +29,22 @@ class PredictivePreFlopEncoder(nn.Module):
         embedding = self.encoder(x)
         equity_metrics = self.decoder(embedding)
         return embedding, equity_metrics
-    
+
+class PreFlopEncoderTriplet(nn.Module):
+    def __init__(self, input_size=22, embedding_dim=8, dropout_rate=0.2):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(64),
+            nn.Dropout(dropout_rate),
+            nn.Linear(64, 32),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(32),
+            nn.Dropout(dropout_rate),
+            nn.Linear(32, embedding_dim)
+        )
+        
+    def forward(self, x):
+        embedding = self.encoder(x)
+        return embedding
